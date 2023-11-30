38c3520bd80f5974737ecbc0fa5ee50a046e191e
on1kou95 <55468088+on1kou95@users.noreply.github.com>
2023-11-30 13:53:57 +0900

    Add files via upload

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-path_folder = '.data/data' #Path to where train and testing lists are located
+path_folder = './data/data' #Path to where train and testing lists are located
 class_mapping = './classes.json'
 weights = 'weights/'
 logs = 'weights/logs/'
